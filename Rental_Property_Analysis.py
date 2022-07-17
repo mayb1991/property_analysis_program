e40adeea7f275d25f41224112b05152041ad3e6e
Bradley May <mayb1991@yahoo.com>
2022-07-17 13:15:00 -0600

    second commit

--- Rental_Property_Analysis.py
+++ Rental_Property_Analysis.py
@@ -6,29 +6,29 @@
 
 class ROI():
     
-    def __init__(self, income, expances, cash_flow, return_on_investment):
-        self.reveune = income
+    def __init__(self, income, expances, cash_flow, return_on_investment,  purchase_price):
+        self.gross = income
         self.exp = expances
         self.flow = cash_flow
         self.return_investment = return_on_investment
+        self.price =  purchase_price
 
     def income(self):
         monthly_rent = int(input("Enter monthly rent: "))
         other_monthly_income = int(input("Please enter the amount of any other income the property may make: "))
-        self.reveune = monthly_rent + other_monthly_income
-        print(f"Your monthly income for this property is ${self.reveune}")
+        self.gross = monthly_rent + other_monthly_income
+        print(f"Your monthly income for this property is ${self.gross}")
         return ROI
 
     def expances (self):
-        vacacy = self.reveune * .10
-        repairs = self.reveune * .07
-        property_manager = self.reveune * .10
-        capex = self.reveune * .05
+        vacacy = self.gross * .10
+        repairs = self.gross * .07
+        property_manager = self.gross * .10
+        capex = self.gross * .05
         monthly_mortage = int(input("What is the expected motrage on the property: "))
         insurance = int(input("If you choose to carry insurance on the property what is your premiuem going to be: "))
         if insurance > 0:
             monthly_insurance = insurance / 12
-            print(monthly_insurance)
         landscape = int(input("If you are planning on paying for lawn care and or snow removeal please enter an amount here: "))
         utilites = int(input("If paying for utilites please enter amount here: "))
         yearly_tax_amount = int(input("Enter amount of annual property taxes: "))
@@ -39,21 +39,52 @@ class ROI():
         print(f"Your total monthly expances for this property are ${self.exp}")
     
     def cash_flow(self):
-        self.flow = self.reveune - self.exp
+        self.flow = self.gross - self.exp
         print(f"Your monthly cash flow for this property is ${self.flow}")
 
     def return_on_investment(self):
-        purchase_price = int(input("Enter the price you are buying the property for: "))
-        down_payment = purchase_price * .20
-        closing_cost = purchase_price * .03
+        self.price = int(input("Enter the price you are buying the property for: "))
+        down_payment = self.price * .20
+        closing_cost = self.price * .03
         rehab = int(input("If there are any repaires need please enter the amount: "))
         misc = int(input("Please enter anyother fees that you may incounter: "))
         self.return_investment = down_payment + closing_cost + rehab + misc
         cash_on_cash = (self.flow / self.return_investment) * 1000
         print(f"Your ROI for this property is {cash_on_cash}%")
 
-h1 = ROI(0,0,0,0)
+    
+
+h1 = ROI(0,0,0,0,0)
 h1.income()
 h1.expances()
 h1.cash_flow()
 h1.return_on_investment()
+
+
+# Here I was trying to create a seprate class for the 1% rule. 
+# To see if the potential propery would pass or not but I can not get the math to work out. 
+# No matter what I do it comes out with everything passing the rule. 
+
+# class Good_Investment_or_not(ROI):
+#     def __init__(self, income, expances, cash_flow, return_on_investment, purchase_price):
+#         super().__init__(income, expances, cash_flow, return_on_investment, purchase_price)
+    
+
+#     def one_rule(self):
+#         one_percent_rule_meaning = """
+#         The 1% rule of real estate investing measures the price of the investment 
+#         property against the gross income it will generate. For a potential investment 
+#         to pass the 1% rule, its monthly rent must be equal to or no less than 1% of the purchase price.
+#         To calculate the 1% rule, simply multiply the property purchase price by 1%. 
+#         The result is the minimum monthly rent that the home should generate.
+#         """
+
+#         one_percent_rule = self.price * .01
+#         print(one_percent_rule)
+#         if one_percent_rule >= self.gross:
+#             print("This property passes the 1% rule!")
+#         else:
+#             print("This property does not pass the 1% rule")
+    
+# h1 = Good_Investment_or_not(0,0,0,0,0)
+# h1.one_rule()
